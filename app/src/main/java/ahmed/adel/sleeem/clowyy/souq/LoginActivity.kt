59de98404e2b27e75ff829d6d5c5a6054ef91762
Hager Samir <40361975+hagersamir123@users.noreply.github.com>
2021-04-27 09:19:39 -0700

    login page

---
+++ app/src/main/java/ahmed/adel/sleeem/clowyy/souq/LoginActivity.kt
@@ -0,0 +1,11 @@
+package ahmed.adel.sleeem.clowyy.souq
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
