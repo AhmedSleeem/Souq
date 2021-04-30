0bb692093e009cfd52f89b9412cc43b4cabca4cf
Taha Khalefah <tahakhalefah@gmail.com>
2021-04-30 15:27:45 +0200

    commit two

---
+++ app/src/main/java/ahmed/adel/sleeem/clowyy/souq/ReviewActivity.kt
@@ -0,0 +1,25 @@
+package ahmed.adel.sleeem.clowyy.souq
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+
+class ReviewActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_review)
+        setStatusBarColor()
+    }
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun setStatusBarColor() {
+        val window: Window = window
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+    }
+}
