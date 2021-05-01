90777a4c67e55e34eedc408e7e12d5528e80c56f
Mnem Elmorshidy <mn3mmoghazii@gmail.com>
2021-05-01 22:13:28 +0200

    add strike

--- app/src/main/java/ahmed/adel/sleeem/clowyy/souq/adapter/FavouriteAdapter.kt
+++ app/src/main/java/ahmed/adel/sleeem/clowyy/souq/adapter/FavouriteAdapter.kt
@@ -3,6 +3,7 @@ package ahmed.adel.sleeem.clowyy.souq.adapter
 import ahmed.adel.sleeem.clowyy.souq.R
 import ahmed.adel.sleeem.clowyy.souq.model.FavouriteItem
 import android.content.Context
+import android.graphics.Paint
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
@@ -27,6 +28,7 @@ class FavouriteAdapter(var context: Context, var arrayList: ArrayList<FavouriteI
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
         var view: View = View.inflate(context, R.layout.card_view_item_grid, null)
         var productImage: ImageView = view.findViewById(R.id.img_product)
         var productName: TextView = view.findViewById(R.id.tv_product_name)
@@ -42,6 +44,8 @@ class FavouriteAdapter(var context: Context, var arrayList: ArrayList<FavouriteI
         productCost.text = listItem.productCost
         productOldCost.text = listItem.productOldCost
         offerPercentage.text = listItem.offerPercentage
+
+        productOldCost.setPaintFlags(productOldCost.getPaintFlags() or Paint.STRIKE_THRU_TEXT_FLAG)
 
         return view
     }
